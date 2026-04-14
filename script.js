62ddada4bf4455fa32c200663a6d4b93ed282f87
bella-s-4 <songi0085@my.sduhsd.net>
2026-04-14 20:29:36 +0000

    Project Description & Mock-up (late because I was out sick on 4/14/26 aka yesterday and couldn't do it) as well as adding some organization to my work including check boxes and input text. Tried to make the percentage be next to the category but failed, so should review code for that later. Edited in html, js, and css.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // add javascript here
+
+//in class tips
+
+// document.getElementById("playBtn").addEventListener("click",function() 
+//     {play(level)
+//     //can also just leave out "function()"
+// })
+
+// function play(level) {
+//     let radios = document.getElementByName("level");
+//     //etc etc
+// }
